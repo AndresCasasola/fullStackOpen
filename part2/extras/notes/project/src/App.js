deee39dd5fb8abbda06f5c31c588d0031ca7669b
Andres Casasola <andres97dominguez@gmail.com>
2023-07-18 13:06:30 +0200

    Update notes up to section d

--- part2/extras/notes/project/src/App.js
+++ part2/extras/notes/project/src/App.js
@@ -1,60 +1,59 @@
 import React, {useState, useEffect} from 'react'
 import './App.css';
 import Note from './components/note'
-import axios from 'axios'
+import noteService from './services/notes'
 
-const App = (props) => {
+const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, SetNewNote] = useState('A new note...')
   const [firstClick, SetFirstClick] = useState(true)
   const [showAll, SetShowAll] = useState(true)
 
-  const addNote = (event) => {
-    event.preventDefault()
-    console.log('EVENT: submit', event.target)
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1
-    }
-    const newNotes = notes.concat(noteObject)
-    setNotes(newNotes)
-    SetNewNote('')
+useEffect(() => { noteService.getAll().then(initNotes  => setNotes(initNotes)) }, [])
+
+const addNote = (event) => {
+  event.preventDefault()
+  console.log('EVENT: submit', event.target)
+
+  const noteObject = {
+    id: notes.length + 1,
+    content: newNote,
+    date: new Date().toISOString(),
+    important: Math.random() < 0.5
   }
 
-  const handleNoteChange = (event) => {
-    console.log('EVENT: input changed ; Value: ', event.target.value)
-    SetNewNote(event.target.value)
-  }
+  noteService
+    .create(noteObject)
+    .then(createdNote => {
+      setNotes(notes.concat(createdNote))
+      SetNewNote('')
+  })
+}
+
+  const handleNoteChange = (event) => SetNewNote(event.target.value)
 
   const handleClick = (event) => {
-    console.log('EVENT: input clicked')
     if(firstClick){
       SetNewNote('')
       SetFirstClick(false)
     } 
   }
 
-  const handleSubmitButton = (event) => {
-    console.log('Submit button clicked...')
-  }
-
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
-  useEffect(() => 
-  {
-    console.log('Effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => 
-      {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important}
 
-  console.log('render', notes.length, 'notes')
+    noteService
+      .update(id, changedNote).then(updatedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : updatedNote))
+      })
+      .catch(error => {
+        alert(`the note ${note.content} was already deleted from server`)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
 
   return (
     <div>
@@ -65,7 +64,13 @@ const App = (props) => {
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => <Note key={note.id} note={note}/>)}
+        {notesToShow.map(note => 
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
+        )}
       </ul>
       <form onSubmit={addNote}>
         <input 
@@ -73,7 +78,7 @@ const App = (props) => {
           onChange={handleNoteChange}
           onClick={handleClick}
         />
-        <button type="submit" onClick={handleSubmitButton}>save</button>
+        <button type="submit">save</button>
       </form>   
     </div>
   )
